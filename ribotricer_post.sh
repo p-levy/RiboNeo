9061894533924e7e4b7823f5492bca2418f2ba43
Pierre Levy <pierrelevy08@gmail.com>
2022-05-24 10:48:33 +0200

    added help/usage

--- ribotricer_post.sh
+++ ribotricer_post.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Script for processing ribotricer output (after reformat step) to compute non-canonical specific read counts
+
+if [ $# -eq 0  ] || [ $1 == "-h" ] || [ $1 == "--help" ]
+then { printf "\n Usage: \n \$1=reformatted unique translating orfs \
+\n \$2=prefix \n \$3=pos wig \n \$4=neg wig \n\n"; exit 1; }
+fi
+
 $(dirname $0)/create_bed.R $1 $2
 
 bedtools subtract -b $2_annotated_orfs.bed -a $2_non_annotated_orfs.bed -s > $2_novel.bed
