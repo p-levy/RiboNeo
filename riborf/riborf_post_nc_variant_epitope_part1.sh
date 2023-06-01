4706273f72a9d2ebfdee04af857bc3b6c786c8a0
Pierre Levy <levy.pierre@yahoo.fr>
2023-06-01 16:30:18 +0200

    fixed sort

--- riborf/riborf_post_nc_variant_epitope_part1.sh
+++ riborf/riborf_post_nc_variant_epitope_part1.sh
@@ -29,8 +29,10 @@ cat $1 | cut -f 1 | grep -v orfID > $2/translating_ORF_ID.txt
 
 # Selected candidate ORFs only translating: candidate_orfs_translating.tsv
 # Using join command (need to sort the id file and table file first)
-
-join -t$'\t' <(sort -d $2/translating_ORF_ID.txt) <(sort -d -t$'\t' -k1,1 $3) > $2/translating.candidateORF.genepred.txt
+sort -d $2/translating_ORF_ID.txt > $2/translating_ORF_ID.sorted.txt
+sort -d -t$'\t' -k1,1 $3 > $2/candidateORF.genepred.sorted.txt
+join -t$'\t' $2/translating_ORF_ID.sorted.txt $2/candidateORF.genepred.sorted.txt > $2/translating.candidateORF.genepred.txt
+# join -t$'\t' <(sort -d $2/translating_ORF_ID.txt) <(sort -d -t$'\t' -k1,1 $3) > $2/translating.candidateORF.genepred.txt
 
 ####### STEP 2: Generate candidate ORF fasta filtered for translating ORFs only ######################################################################
 
