2ef666af4ed2e44109b4e8ef37edeea17b6d9600
Pierre Levy <levy.pierre@yahoo.fr>
2023-06-01 17:03:11 +0200

    fix sort

--- riborf/riborf_post_nc_variant_epitope_part1.sh
+++ riborf/riborf_post_nc_variant_epitope_part1.sh
@@ -29,8 +29,8 @@ cat $1 | cut -f 1 | grep -v orfID > $2/translating_ORF_ID.txt
 
 # Selected candidate ORFs only translating: candidate_orfs_translating.tsv
 # Using join command (need to sort the id file and table file first)
-sort -d $2/translating_ORF_ID.txt > $2/translating_ORF_ID.sorted.txt
-sort -d -t$'\t' -k1,1 $3 > $2/candidateORF.genepred.sorted.txt
+sort $2/translating_ORF_ID.txt -k1d -k2,3n -t'|' > $2/translating_ORF_ID.sorted.txt
+sort -k1d -k2,3n -t'|' $3 > $2/candidateORF.genepred.sorted.txt
 join -t$'\t' $2/translating_ORF_ID.sorted.txt $2/candidateORF.genepred.sorted.txt > $2/translating.candidateORF.genepred.txt
 # join -t$'\t' <(sort -d $2/translating_ORF_ID.txt) <(sort -d -t$'\t' -k1,1 $3) > $2/translating.candidateORF.genepred.txt
 
